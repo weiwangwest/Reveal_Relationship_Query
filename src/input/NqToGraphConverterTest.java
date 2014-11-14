4eeea76b23d8621445f248ed6cdd25e4cc0d8db8
weiwangwest <weiwang@uni-koblenz.de>
2014-11-14 09:54:03 +0100

    add DatasetLoaderFromMapReplacedFile

--- src/input/NqToGraphConverterTest.java
+++ src/input/NqToGraphConverterTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.JUnitCore;
 import fundamental.FileNameManager;
 
 public class NqToGraphConverterTest {
+	final int lastFileNo=6;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -17,9 +18,8 @@ public class NqToGraphConverterTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 	@Test
-	public void generateFilesDifferentEachOtherTest() throws Exception {
+	public void generateMultipleMapFilesTest() throws Exception{
 		int index=0;
-		int lastFileNo=6;
 		//generate difference files and map files
 		NqToGraphConverter.generateFilesDifferentEachOther(FileNameManager.getGzipFileNames("vertex", 0, lastFileNo));		
 		index = NqToGraphConverter.generateMultipleMapFiles(FileNameManager.getGzipFileNames("vertex.final", 0, lastFileNo), index);
@@ -31,8 +31,10 @@ public class NqToGraphConverterTest {
 
 		NqToGraphConverter.generateFilesDifferentEachOther(FileNameManager.getGzipFileNames("subgraph", 0, lastFileNo));
 		index =NqToGraphConverter.generateMultipleMapFiles(FileNameManager.getGzipFileNames("subgraph.final", 0, lastFileNo), index);
-		System.out.println("maxmun index="+index);
-
+		System.out.println("maxmun index="+index);		
+	}
+	@Test
+	public void generateFilesDifferentEachOtherTest() throws Exception {
 		//use the map to replace the lines file
 		NqToGraphConverter.generateMapsReplacedFiles(FileNameManager.getGzipFileNames("vertex.final.map.final", 0, lastFileNo), 
 				FileNameManager.getGzipFileNames("line", 0, lastFileNo), NqToGraphConverter.VERTEX_TYPE_OF_REPLACEMENT);
@@ -43,6 +45,12 @@ public class NqToGraphConverterTest {
 		//check if they are totally mapped.
 		assertTrue(NqToGraphConverter.areNqFilesTotallyMapped(FileNameManager.getGzipFileNames("line", 0, lastFileNo)));
 	}
+	@Test
+	public void loadGraphFromGeneratedData() throws Exception {
+		//generate difference files and map files
+		for (int i=0; i<lastFileNo; i++){	
+		}
+	}		
 	public static void main(String[] args) throws Exception {                    
 	       JUnitCore.main("input.NqToGraphConverterTest"); 
 	}
